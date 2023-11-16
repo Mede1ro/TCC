12957e72c904ed9af4dfe59cbbf472b1811337f7
lufucx <luizsilva.miui@gmail.com>
2023-11-16 01:02:30 -0300

    Adicionando os conflitos.

---
+++ p/ar1.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0" />
+    <title>Conflito</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <a class="cabecalho-imagem" href="../index.php"><img src="../assets/logo.svg" alt="Logo do IFRJ">Observatório
+            dos Conflitos Ambientais</a>
+        <nav class="cabecalho-menu ">
+            <li class="cabecalho-menu-item">
+                <a class="cabecalho-menu-item-links" href="../index.php">Inicio</a>
+            </li>
+            <li class="cabecalho-menu-item">
+                <a class="cabecalho-menu-item-links" href="../pages/atualizacoes.php">Atualizações</a>
+            </li>
+            <li class="cabecalho-menu-item">
+                <a class="cabecalho-menu-item-links" href="../pages/sobre.php">Sobre</a>
+            </li>
+            <li class="cabecalho-menu-item">
+                <a class="cabecalho-menu-item-links" href="../pages/contato.php">Contato</a>
+            </li>
+        </nav>
+    </header>
+
+    <main class="container-conflitos">
+        <h1 class="conteudo-conflitos-titulo">A urbanização da orla da laguna de Araruama 
+            <br>
+            em Araruama e seus impactos em São Pedro</h1>
+
+        <div class="conteudo-conflitos-texto">
+            <p>
+
+                <br><br>
+                O Ministério Público Federal em São Pedro da Aldeia, no Rio de Janeiro (MPF/RJ), firmou termos de
+                ajustamento de conduta (TACs) com prefeituras da Região dos Lagos para a desocupação, interdição e
+                demolição de quiosques irregularmente instalados nas praias com a remoção dos entulhos provenientes. Os
+                Municípios de Búzios, Arraial do Cabo e Cabo Frio se comprometeram a retirar 64 construções irregulares
+                logo após o carnaval. O acordo ocorre paralelo à decisão judicial, em ação movida pelo MPF/RJ, que já
+                havia determinado a imediata retirada dos quiosques.
+                <br><br>
+                Os proprietários dos quiosques na Praia do Sol, em São Pedro da Aldeia, estão buscando alternativas para
+                reaver a permanência de suas atividades no local. No fim de agosto deste ano, a Prefeitura agendou uma
+                reunião com os permissionários e, na ocasião, os informou que o Ministério Público havia pedido a
+                desocupação da área. Mas como não ficou claro de quem partiu a exigência de retirada dos quiosques, os
+                quiosqueiros entraram com ações judiciais contra o município pedindo explicações.
+                <br><br>
+
+
+            </p>
+
+        </div>
+
+
+        <img class="imagem-conflitos" src="">
+
+
+        <div class="conteudo-conflitos-texto">
+
+            <p>
+
+                <br><br>
+                “Até o momento não foi oportunizado aos quiosqueiros exercerem a ampla defesa e, por isso, estamos
+                ingressando na justiça”, disse, informando que o mesmo acontece com outras praias de São Pedro da
+                Aldeia, que também estão com o mesmo problema.
+                <br><br>
+                “Após um longo período de “lock down”, os quiosqueiros estão esperando a alta temporada para trabalhar.
+                A orla de São Pedro da Aldeia é muito procurada por turistas na alta estação, e a alta temporada sempre
+                foi ganha pão de centenas de pessoas na Região dos Lagos. É um problema social e de direito coletivo,
+                que deve ser tratado com muita atenção pelas autoridades locais e o judiciário”, destacou o advogado
+                ressaltando que todos os quiosques possuem abastecimento de água da concessionária, bem como sistema de
+                esgoto.
+                <br><br>
+                “Até o momento não foi oportunizado aos quiosqueiros exercerem a ampla defesa e, por isso, estamos
+                ingressando na justiça”, disse, informando que o mesmo acontece com outras praias de São Pedro da
+                Aldeia, que também estão com o mesmo problema.
+                <br><br>
+                “Após um longo período de “lock down”, os quiosqueiros estão esperando a alta temporada para trabalhar.
+                A orla de São Pedro da Aldeia é muito procurada por turistas na alta estação, e a alta temporada sempre
+                foi ganha pão de centenas de pessoas na Região dos Lagos. É um problema social e de direito coletivo,
+                que deve ser tratado com muita atenção pelas autoridades locais e o judiciário”, destacou o advogado
+                ressaltando que todos os quiosques possuem abastecimento de água da concessionária, bem como sistema de
+                esgoto.
+                <br><br>
+
+                Relato de Morador: "Estão com essa história de demolirem nossos quiosques novamente, toda vez que muda
+                de prefeito escutamos isso, o problema é que eles querem demolir tudo e não estão "nem aí" pra gente"
+                <br><br>
+
+                "Então essa situação não é recente?"
+                <br><br>
+                Relato de Morador: "Não mesmo, isso vem acontecendo há anos, e nunca dá em nada, só preocupação pra
+                gente, que ficamos com medo de perder nosso único sustento."
+                <br><br>
+                <span>FONTES:</span>
+                <br><br>
+                <a class="links-conflitos"
+                    href="https://odia.ig.com.br/colunas/politica-costa-do-sol/2022/04/6376900-mpf-entra-com-acao-obrigando-prefeitura-aldeense-a-demolir-quiosques-nas-praias.html">
+                    https://odia.ig.com.br/colunas/politica-costa-do-sol/2022/04/6376900-mpf-entra-com-acao-obrigando-prefeitura-aldeense-a-demolir-quiosques-nas-praias.html</a>
+                <br><br>
+                <a class="links-conflitos"
+                    href="https://mpf.jusbrasil.com.br/noticias/113795909/mpf-rj-assina-acordos-para-demolicao-de-64-quiosques-na-regiao-dos-lagos">
+                    https://mpf.jusbrasil.com.br/noticias/113795909/mpf-rj-assina-acordos-para-demolicao-de-64-quiosques-na-regiao-dos-lagos</a>
+                <br><br>
+
+                <a class="links-conflitos"
+                    href="https://fontecerta.com/quiosqueiros-da-praia-do-sol-questionam-prefeitura-de-sao-pedro-sobre-pedido-de-desocupacao-da-area/">
+                    https://fontecerta.com/quiosqueiros-da-praia-do-sol-questionam-prefeitura-de-sao-pedro-sobre-pedido-de-desocupacao-da-area/</a>
+
+
+                <br><br>
+                <a class="links-conflitos"
+                    href="https://fontecerta.com/quiosqueiros-da-praia-do-sol-questionam-prefeitura-de-sao-pedro-sobre-pedido-de-desocupacao-da-area/">
+                    https://fontecerta.com/quiosqueiros-da-praia-do-sol-questionam-prefeitura-de-sao-pedro-sobre-pedido-de-desocupacao-da-area/</a>
+
+                <br><br>
+
+            </p>
+
+        </div>
+
+        <iframe class="video-conflitos" width="800" height="300" src="https://www.youtube.com/embed/vPdG7Q6UGyI"
+            title="Quiosques demolidos em praia de Cabo Frio" frameborder="0"
+            allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+
+        </div>
+        </div>
+        </form>
+
+
+    </main>
+
+    <footer class="rodape">
+
+    </footer>
+
+
+
+</body>
+
+</html>
