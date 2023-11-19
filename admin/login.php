9c2c180bdfbc370ec83fcb46c4a6077c0f5814f9
Mede1ros <mede1ro@proton.me>
2023-11-18 21:57:06 -0300

    Página de Contato Feita

--- admin/login.php
+++ admin/login.php
@@ -1,32 +1,31 @@
 <?php
 include_once "../pages/conectar.php";
 
-session_start();
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['usuario']) && isset($_POST['senha'])) {
-        $usuarios = $_POST['usuario'];
+        $usuario = $_POST['usuario'];
         $senha = $_POST['senha'];
 
-        $query_login = "SELECT * FROM admin WHERE usuario = ? AND senha = ?";
+        $query_login = "SELECT * FROM ADMIN WHERE usuario = ? AND senha = ?";
 
         $stmt = $conexao->prepare($query_login);
-        $stmt->bind_param("ss", $usuarios, $senha);
+        $stmt->bind_param("ss", $usuario, $senha);
 
+        // Executar a consulta
         $stmt->execute();
 
+        // Obter resultado
         $resultado = $stmt->get_result();
 
         if ($resultado->num_rows > 0) {
-            $_SESSION['usuarios'] = $usuarios;
-            $_SESSION['nome'] = $row->nome;
-            header('Location: dashboard/dashboard.php');
+            // As credenciais estão corretas, redirecione para a página de admin
+            header('Location: dashboard/dashboard.html');
             exit;
         } else {
-            echo "Usuário e/ou senha incorreto(s). Tente novamente.";
-            header('Location: index.html');
+            echo "Usuário ou senha incorretos. Tente novamente.";
         }
 
+        // Fechar a declaração preparada
         $stmt->close();
     } else {
         echo "Por favor, preencha todos os campos.";
@@ -35,3 +34,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $conexao->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <title>Admin</title>
+</head>
+
+<body>
+    <div class="container-login">
+        <div class="container-login-background">
+            <div class="card-conteudo">
+                <div class="card-conteudo-login">
+                    <div class="card-conteudo-login-header">
+                        <img src="../assets/logo.svg" alt="Logo IFRJ">
+                        <h1>Login<span class="card-conteudo-login-header-barra"></span>
+                            <span class="card-conteudo-login-header-admin">Administrador<span>
+                        </h1>
+                    </div>
+                    <form class="card-conteudo-login-form" method="post" action="login.php">
+                        <div class="text-input">
+                            <i class="ri-user-fill"></i>
+                            <input type="text" name="usuario" id="usuario" placeholder="Usuário">
+                        </div>
+
+                        <div class="text-input">
+                            <i class="ri-lock-fill"></i>
+                            <input type="password" name="senha" id="senha" placeholder="Senha">
+                        </div>
+                        <input type="submit" name="enviar" value="Enviar">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
--- pages/sobre.php
+++ pages/sobre.php
@@ -7,8 +7,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/style.css">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0" />
   <title>Document</title>
 </head>
 
@@ -35,11 +34,32 @@
 
   <main class="container-sobre">
     <div class="conteudo-sobre">
-      <div class="conteudo-sobre-background">
-        <img src="../assets/bgsobre" alt="">
+      <div class="conteudo-sobre-bg">
+        <img src="../assets/bgsobre.png" alt="">
+        <h1>Sobre</h1>
       </div>
-      <h1>Sobre</h1>
-      <p>O nosso site, "Conflitos Ambientais na Região dos Lagos," não é apenas um repositório de informações, mas uma voz ativa na defesa da região e do nosso planeta. Ele é uma chamada à ação, um apelo à responsabilidade e uma inspiração para mudanças significativas. Junte-se a nós para proteger a beleza única da região dos Lagos e trabalhar juntos em direção a um futuro mais sustentável, onde o equilíbrio entre o desenvolvimento e a conservação seja uma realidade duradoura. O tempo de agir é agora, e você faz parte dessa solução.</p>
+      <div class="conteudo-sobre-texto">
+        <h1><img class="leaf" src="../assets/leaf.svg" alt=""><span style="color: #4baf47;">Sobre o Projeto:</span> Conflitos Ambientais na Região dos Lagos</h1>
+        <p>Bem-vindo ao nosso espaço dedicado à análise e compreensão dos conflitos ambientais na região dos lagos, uma iniciativa desenvolvida como parte do Trabalho de Conclusão de Curso no Instituto Federal do Rio de Janeiro, Campus Campos Arraial do Cabo.</p>
+        <br>
+
+        <h2>Objetivo do Projeto:</h2>
+        <p>O principal propósito deste site é lançar luz sobre os diversos desafios ambientais que afetam a região dos lagos. Buscamos conscientizar o público sobre a complexidade dessas questões, oferecendo informações detalhadas e análises críticas. Nosso foco está na apresentação objetiva dos problemas ambientais, a fim de promover uma compreensão mais profunda e informada sobre os desafios enfrentados na região.</p>
+        <br>
+        <h2>Contexto:</h2>
+        <p>A região dos lagos, conhecida por sua beleza natural e biodiversidade, enfrenta uma série de desafios ambientais que ameaçam não apenas a fauna e flora locais, mas também a qualidade de vida das comunidades que dependem desses ecossistemas. Desde questões relacionadas à poluição da água até o impacto das atividades humanas na área, cada aspecto é explorado com o objetivo de fornecer uma visão abrangente e informada.</p>
+        <br>
+        <h2>Metodologia:</h2>
+        <p>Nosso trabalho se baseia em uma pesquisa aprofundada, combinando fontes científicas, relatos locais e dados coletados in loco. Ao adotar uma abordagem multidisciplinar, buscamos apresentar uma visão holística dos conflitos ambientais na região dos lagos.</p>
+        <br>
+        <h2>Compromisso com a Conscientização:</h2>
+        <p>Acreditamos que a conscientização é o primeiro passo para a mudança. Ao apresentar informações de maneira acessível e envolvente, esperamos inspirar ações positivas e incentivar a comunidade a participar ativamente na preservação e recuperação do ambiente.</p>
+        <br>
+        <h2>Participe:</h2>
+        <p>Este espaço não é apenas um repositório de informações, mas um convite à participação. Encorajamos a interação, sugestões e compartilhamento de experiências. Juntos, podemos contribuir para um futuro mais sustentável para a região dos lagos.</p>
+        <br>
+        <h3>Agradecemos por se juntar a nós nesta jornada de descoberta e conscientização.</h3>
+      </div>
     </div>
   </main>
 
